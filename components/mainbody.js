ec175ff2d0afaf8f30d54a76b85776b4bb78a91f
gccornejo441 <gcornejo441@gmail.com>
2021-06-19 09:29:51 -0700

    Mobile navi set

--- components/mainbody.js
+++ components/mainbody.js
@@ -24,50 +24,53 @@ const MainBody = () => {
                     </div>
                 </div>
                 <div>
-                    <div class="flex justify-center p-20 text-blue-900">
+                    <div class="flex justify-evenly p-20 text-blue-900">
                         No Data
                     </div>
                 </div>
             </div>
-            <div class="grid grid-cols-3 gap-7 mt-5">
-                <div class="flex bg-white rounded-lg shadow-md">
-                    <div class="bg-gray-400 p-4 mx-20 my-10 rounded-lg">
+            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-7 mt-5">
+                <div class="lg:flex justify-evenly bg-white rounded-lg shadow-md">
+                    <div class="bg-gray-400 p-4 my-5 ml-5 lg:my-10 rounded-lg w-max">
                         <UserGroupIcon class="h-7 w-7 text-gray-800" />
                     </div>
                     <div class="">
-                        <ul class="relative top-5">
-                            <li class="font-normal text-gray-400 text-sm">Customers</li>
-                            <li class="font-semibold text-blue-900 text-3xl">345k</li>
-                            <li class="font-light text-gray-400 text-xs">Feb 1 - Apr 1</li>
-                            <li class="font-light text-blue-900 text-xs flex">Since last month<span class="pl-1 text-green-500"><ChevronUpIcon class="w-4 h-4"/></span><span class="text-green-500">22%</span></li>
+                        <ul class="relative lg:top-5 my-5 lg:my-0 ml-5">
+                            <li class="font-normal text-gray-400 text-sm md:text-md">Customers</li>
+                            <li class="font-semibold text-blue-900 text-3xl md:text-4xl">345k</li>
+                            <li class="font-light text-gray-400 text-xs md:text-sm">Feb 1 - Apr 1</li>
+                            <li class="font-light text-blue-900 text-xs md:text-sm flex w-max">Since last month<span class="pl-1 text-green-500"><ChevronUpIcon class="w-4 h-4"/></span><span class="text-green-500">22%</span></li>
                         </ul>
                     </div>
+                    <div></div>
                 </div>
-                    <div class="flex bg-white rounded-lg shadow-md">
-                    <div class="bg-salmon p-4 mx-20 my-10 rounded-lg">
+                    <div class="lg:flex justify-evenly bg-white rounded-lg shadow-md">
+                    <div class="bg-salmon p-4 my-5 ml-5 lg:my-10 rounded-lg w-max">
                         <ShoppingBagIcon class="h-7 w-7 text-yellow-400" />
                     </div>
                     <div class="">
-                        <ul class="relative top-5">
-                            <li class="font-normal text-gray-400 text-sm">Revenue</li>
-                            <li class="font-semibold text-blue-900 text-3xl">$43,594</li>
-                            <li class="font-light text-gray-400 text-xs">Feb 1 - Apr 1</li>
-                            <li class="font-light text-blue-900 text-xs flex">Since last month<span class="pl-1 text-red-500"><ChevronDownIcon class="w-4 h-4"/></span><span class="text-red-500">2%</span></li>
+                        <ul class="relative lg:top-5 my-5 lg:my-0 ml-5">
+                            <li class="font-normal text-gray-400 text-sm md:text-md">Revenue</li>
+                            <li class="font-semibold text-blue-900 text-3xl md:text-4xl">$43,594</li>
+                            <li class="font-light text-gray-400 text-xs md:text-sm">Feb 1 - Apr 1</li>
+                            <li class="font-light text-blue-900 text-xs md:text-sm flex w-max">Since last month<span class="pl-1 text-red-500"><ChevronDownIcon class="w-4 h-4"/></span><span class="text-red-500">2%</span></li>
                         </ul>
                     </div>
+                    <div></div>
                 </div>
-                    <div class="flex bg-white rounded-lg shadow-md">
-                    <div class="bg-violet-200 p-4 mx-20 my-10 rounded-lg">
+                    <div class="lg:flex justify-evenly bg-white rounded-lg shadow-md">
+                    <div class="bg-violet-200 p-4 my-5 ml-5 lg:my-10 rounded-lg w-max">
                         <PresentationChartLineIcon class="h-7 w-7 text-violet-500" />
                     </div>
                     <div class="">
-                        <ul class="relative top-5">
-                            <li class="font-normal text-gray-400 text-sm">Bounce Rate</li>
-                            <li class="font-semibold text-blue-900 text-3xl">50.88%</li>
-                            <li class="font-light text-gray-400 text-xs">Feb 1 - Apr 1</li>
-                            <li class="font-light text-blue-900 text-xs flex">Since last month<span class="pl-1 text-green-500"><ChevronUpIcon class="w-4 h-4"/></span><span class="text-green-500">4%</span></li>
+                        <ul class="relative lg:top-5 my-5 lg:my-0 ml-5">
+                            <li class="font-normal text-gray-400 text-sm md:text-md">Bounce Rate</li>
+                            <li class="font-semibold text-blue-900 text-3xl md:text-4xl">50.88%</li>
+                            <li class="font-light text-gray-400 text-xs md:text-sm">Feb 1 - Apr 1</li>
+                            <li class="font-light text-blue-900 text-xs md:text-sm flex w-max">Since last month<span class="pl-1 text-green-500"><ChevronUpIcon class="w-4 h-4"/></span><span class="text-green-500">4%</span></li>
                         </ul>
                     </div>
+                    <div></div>
                 </div>
             </div>
         </>
--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,25 @@ import Head from '../components/head';
 // import Products from '../components/products';
 import SideNav from '../components/sidenav';
 import SearchBar from '../components/searchbar';
-import MainBody from '../components/mainbody';
+import MobileNav from '../components/mobilenav';
 
 const Index = () => {
 
   return (
-  <>
-  <Head/>
-        <div class="flex">
-          <div class="h-screen px-4 pt-3 bg-blue-900 grid place-content-between">
-            <SideNav/>
-          </div>
-          <div class="w-screen">
-          <SearchBar/>
-          </div>
+    <>
+      <Head />
+      <div class="md:hidden">
+        <MobileNav />
+      </div>
+      <div class="flex">
+        <div class="h-screen px-4 pt-3 bg-blue-900 grid place-content-between hidden md:block">
+          <SideNav />
         </div>
-  </>
+        <div class="w-screen">
+          <SearchBar />
+        </div>
+      </div>
+    </>
   )
 };
 
