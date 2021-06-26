4090eeece84a6e6d4a6dd2da7877a04f2348d4e8
gccornejo441 <gcornejo441@gmail.com>
2021-06-26 16:02:51 -0700

    nextjs-auth0 package installed for easy user auth

--- components/navbar.js
+++ components/navbar.js
@@ -1,8 +1,7 @@
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 import { CloudIcon } from '@heroicons/react/solid'
-import SideNav from './sidenav';
-import MobileNavView from './mobilenavView';
+import { useUser } from '@auth0/nextjs-auth0';
 
 const navigation = [
   { name: 'Dashboard', href: '#', current: true },
@@ -16,68 +15,74 @@ function classNames(...classes) {
 }
 
 export default function Navbar() {
-    return (
-        <>
-          <Disclosure as="nav" class="bg-white">
-            {({ open }) => (
-              <>
-                <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-                  <div class="relative flex items-center justify-between h-16">
-                  
-                  {/* RACKET SPACE LOG */}
-                    <div class="flex">
-                      <span class="p-2 text-green-300">
-                        <CloudIcon class="h-6 w-6" />
-                      </span>
-                      <span class="text-regal-blue text-xl p-2">
-                      Jobster
-                      </span>
-                    </div>
-                  {/* RACKET SPACE LOG */}
-                  <div class="flex">
-                  {/* RACKET SPACE SIGN-UP */}
-                  <div class="flex hidden md:flex mx-3">
-                      <a href="./user/sign-in" class="text-regal-blue border rounded-md bg-green-300 text-regal-blue text-xl p-2">
-                        Sign In
-                      </a>
-                    </div>
-                  {/* RACKET SPACE SIGN-UP */}
-                  {/* RACKET SPACE LOG IN*/}
-                  <div class="flex hidden md:flex">
-                      <a href="./user/registration" class="text-regal-blue border rounded-md bg-green-300 text-regal-blue text-xl p-2">
-                        Register
-                      </a>
-                    </div>
-                  {/* RACKET SPACE LOG IN*/}
+  const { user } = useUser();
 
-                  </div>
-    
-                    <div class="absolute inset-y-0 right-0 flex items-center lg:hidden">
-                      {/* Mobile menu button*/}
-                      <Disclosure.Button class="inline-flex items-center justify-center p-2 rounded-md text-regal-blue hover:text-regal-blue hover:bg-green-300 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                        <span class="sr-only">Open main menu</span>
-                        {open ? (
-                          <XIcon class="block h-6 w-6 fixed z-30 hover:bg-regal-blue hover:text-regal-blue" aria-hidden="true" />
-                        ) : (
-                          <MenuIcon class="block h-6 w-6" aria-hidden="true" />
-                        )}
-                      </Disclosure.Button>
-                    </div>
-                  </div>
+  return (
+    <>
+      <Disclosure as="nav" class="bg-white">
+        {({ open }) => (
+          <>
+            <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+              <div class="relative flex items-center justify-between h-16">
+                {/* RACKET SPACE LOGO */}
+                <div class="flex">
+                  <span class="p-2 text-green-300">
+                    <CloudIcon class="h-6 w-6" />
+                  </span>
+                  <span class="text-regal-blue text-xl p-2">
+                    Jobster
+                  </span>
                 </div>
-    
-                <Disclosure.Panel class="lg:hidden">
-                <div class="flex h-screen relative sm:hidden">
-                      <a href="./dashboard" class="text-regal-blue border rounded-md bg-green-300 text-regal-blue text-xl p-2 mx-auto h-12">
-                        Sign In
-                      </a>
-                    </div>
-                </Disclosure.Panel>
-              </>
-            )}
-          </Disclosure>
-          <div class="w-screen bg-regal-blue h-2">
-          </div>
-        </>
-      )
+
+                {/* RACKET SPACE LOG */}
+                <div class="flex">
+                  {user ? (
+                    <>
+                      <li class="flex hidden md:flex mx-3">
+                        <a href="/api/auth/logout" data-testid="logout" class="text-regal-blue border rounded-md bg-green-300 text-regal-blue text-xl p-2">
+                          Logout
+                        </a>
+                      </li>
+                    </>
+                  ) : (
+                    <>
+                      <li class="flex hidden md:flex mx-3">
+                        <a href="/api/auth/login" data-testid="login" class="text-regal-blue border rounded-md bg-green-300 text-regal-blue text-xl p-2">
+                          Login
+                        </a>
+                      </li>
+                    </>
+                  )}
+
+
+                </div>
+
+                <div class="absolute inset-y-0 right-0 flex items-center lg:hidden">
+                  {/* Mobile menu button*/}
+                  <Disclosure.Button class="inline-flex items-center justify-center p-2 rounded-md text-regal-blue hover:text-regal-blue hover:bg-green-300 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                    <span class="sr-only">Open main menu</span>
+                    {open ? (
+                      <XIcon class="block h-6 w-6 fixed z-30 hover:bg-regal-blue hover:text-regal-blue" aria-hidden="true" />
+                    ) : (
+                      <MenuIcon class="block h-6 w-6" aria-hidden="true" />
+                    )}
+                  </Disclosure.Button>
+                </div>
+              </div>
+            </div>
+
+            <Disclosure.Panel class="lg:hidden">
+              <div class="flex h-screen relative sm:hidden">
+                <a href="./dashboard" class="text-regal-blue border rounded-md bg-green-300 text-regal-blue text-xl p-2 mx-auto h-12">
+                  Sign In
+                </a>
+              </div>
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
+      <div class="w-screen bg-regal-blue h-2">
+      </div>
+    </>
+  )
 }
--- pages/user/sign-in.js
+++ pages/user/sign-in.js
@@ -4,23 +4,23 @@ const SignIn = () => {
     const { user, error, isLoading } = useUser();
     if (isLoading) return <div>Loading...</div>;
     if (error) return <div>{error.message}</div>;
-    alert("user: ", user.name)
+
     return (
         <>
             <div class="flex justify-center mt-32 md:mt-72">
                 <div class="w-full max-w-xs">
-                    <form action="/api/userbase" method="POST" class="bg-regal-blue shadow-md rounded-md px-8 pt-6 pb-8 mb-4">
+                    <form action="" method="" class="bg-regal-blue shadow-md rounded-md px-8 pt-6 pb-8 mb-4">
                         <div class="mb-4">
                             <label class="block text-gray-50 text-sm font-bold mb-2" for="username">
                                 Username
                             </label>
-                            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-50 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+                            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-900 leading-tight focus:outline-none focus:shadow-outline" name="username" id="username" type="text" placeholder="Username" />
                         </div>
                         <div class="mb-6">
                             <label class="block text-gray-50 text-sm font-bold mb-2" for="password">
                                 Password
                             </label>
-                            <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-50 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="password" id="password" type="password" placeholder="******************" />
+                            <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-900 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="password" id="password" type="password" placeholder="******************" />
                             <p class="text-red-500 text-xs italic">Please choose a password.</p>
                         </div>
                         <div class="flex items-center justify-between">
@@ -28,7 +28,9 @@ const SignIn = () => {
                                 Sign In
                             </button>
                             <div>
-                                Welcome {user.name}! <a href="/api/auth/login" data-testid="login">Login</a>
+                                <a class="bg-green-400 hover:bg-green-300 text-regal-blue font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" href="/api/auth/logout" data-testid="logout">
+                                    Logout
+                                </a>
                             </div>
                             <a class="inline-block align-baseline font-bold text-sm text-gray-50 hover:text-green-300" href="#">
                                 Forgot Password?
