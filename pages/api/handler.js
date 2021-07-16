9573a2f2d3dffbee403843b267aedb15ff47139c
gccornejo441 <gcornejo441@gmail.com>
2021-07-15 22:40:26 -0700

    test for pdf

--- pages/api/handler.js
+++ pages/api/handler.js
@@ -1,6 +1,7 @@
 import { MongoClient } from 'mongodb';
 import PDFDocument from 'pdfkit';
 import fs from "fs";
+import { connectToDatabase } from "../../util/mongodb";
 
 
 const uri = process.env.MONGODB_URI;
@@ -192,29 +193,235 @@ const handler = async (req, res) => {
     ) : (
       res.redirect('/user/profile')
     )
+
   } else if (req.method === 'GET') {
+    // DB Connector
+    const { db } = await connectToDatabase();
+    const resume = await db
+    .collection(process.env.MONGO_USER_COLLECTION)
+    .find()
+    .toArray();
 
     var doc = new PDFDocument();
     doc.pipe(fs.createWriteStream('_resume' + '.pdf'))
-
 
     // and some justified text wrapped into columns
     doc
       .fillColor('black')
       .font('Times-Bold', 16)
       .moveUp(1.6)
-      .text("Gabriel Cornejo", {
+      .text(resume[0].first_name + " " + resume[0].last_name, {
         width: 412,
         align: 'center',
         indent: 30,
         columns: 1,
         height: 500,
         ellipsis: true
+      })
+
+    // and some justified text wrapped into columns
+    doc
+      .fillColor('black')
+      .font('Times-Roman', 12)
+      .moveDown()
+      .moveUp()
+      .text(resume[0].city + " " + resume[0].state + "," + " " + resume[0].zip, {
+        width: 412,
+        align: 'center',
+        indent: 30,
+        columns: 1,
+        ellipsis: true
+      })
+      .text("Phone:" + " " + resume[0].phone + " " + "Email:" + " " + resume[0].email, {
+        width: 412,
+        align: 'center',
+        indent: 30,
+        columns: 1,
+        ellipsis: true
+      })
+      .text("Portfolio:" + " " +  "www." + resume[0].github, {
+        width: 412,
+        align: 'center',
+        indent: 30,
+        columns: 1,
+        ellipsis: true
+      })
+      
+      let box = { about: resume[0].about_me.replace(/\n/g, '') }
+    // and some justified text wrapped into columns
+    doc
+      .fillColor('black')
+      .font('Times-Bold', 12)
+      .moveDown(1)
+      .text("Summary of Qualifications:", {
+        width: 412,
+        align: 'left',
+        indent: 0,
+        columns: 1,
+        height: 500,
+        ellipsis: true
+      })
+      .font('Times-Roman', 12)
+      .text(box.about, {
+        width: 412,
+        align: 'left'
+      })
+
+      doc
+      .moveDown()
+      .text("NodeJS/NPM/Git/Express", { align: 'left'})
+      .moveDown()
+      doc.text("React, NextJS, EJS", { align: 'left'})
+      .moveDown()
+      doc.text("MongoDB/Mongoose", { align: 'left'})
+      
+      doc
+      .moveUp(5)
+      .text("Marketing", { align: 'center'})
+      .moveDown()
+      doc.text("Microsoft Word & Excel", { align: 'center'})
+      .moveDown()
+      doc.text("API Integration", { align: 'center'})
+      
+      doc
+      .moveUp(5)
+      .text("This text is left aligned", { align: 'right'})
+      .moveDown()
+      doc.text("This text is left aligned", { align: 'right'})
+      .moveDown()
+      doc.text("This text is left aligned", { align: 'right'})
+      
+
+    doc
+      .fillColor('black')
+      .font('Times-Bold', 12)
+      .moveDown(1)
+      .text("Experience", {
+        width: 412,
+        align: 'left',
+        indent: 0,
+        columns: 1,
+        height: 500,
+        ellipsis: true
+      })
+
+    doc
+      .fillColor('black')
+      .font('Times-Bold', 12)
+      .moveDown(1)
+      .text(resume[0].job_title.job_1[0] + " " +  resume[0].job_1.job_start.slice(0,4) + "-" + resume[0].job_1.job_end !== "" ? resume[0].job_1.job_end.slice(0,4) : false , {
+        width: 412,
+        align: 'left',
+        indent: 0,
+        columns: 1,
+        height: 500,
+        ellipsis: true
+      })
+    doc
+      .fillColor('black')
+      .font('Times-Italic', 12)
+      .text("United States Marine Platoon Sergeant / Senior Manager", {
+        width: 412,
+        align: 'left',
+        indent: 0,
+        columns: 1,
+        height: 500,
+        ellipsis: true
+      })
+
+    let myArrayOfItems = ['Maintained a total of 54 pieces of Military trucks and equipment: 32 MTVR(s)/7 Ton trucks, 8 MK593/7 Ton Cargo Trailers, 14 M119/Water Bowl(s).',
+      'Briefed top executives on weekly duties', 'In preparation for biannual Executive Officer’s Readiness Inspection; identified and conducted training seminars for untrained team members, updating administrative files on Military trucks and equipment.',
+      'Wrote up military awards and recommendations using Microsoft Word and government interfaces.'];
+
+    doc
+      .font('Times-Roman', 12)
+      .moveDown()
+      .list(myArrayOfItems, {
+        bulletRadius: 2,
+        width: 412,
+        align: 'left',
+        columns: 1,
+        height: 500,
+        ellipsis: true
       });
 
+    doc
+      .fillColor('black')
+      .font('Times-Bold', 12)
+      .moveDown(1)
+      .text("Marine Corps Base Hawaii, Kaneohe Bay HI     2015-2019", {
+        width: 412,
+        align: 'left',
+        indent: 0,
+        columns: 1,
+        height: 500,
+        ellipsis: true
+      })
+
+    doc
+      .fillColor('black')
+      .font('Times-Italic', 12)
+      .text("United States Marine Platoon Sergeant / Senior Manager", {
+        width: 412,
+        align: 'left',
+        indent: 0,
+        columns: 1,
+        height: 500,
+        ellipsis: true
+      })
+
+    doc
+      .font('Times-Roman', 12)
+      .moveDown()
+      .list(myArrayOfItems, {
+        bulletRadius: 2,
+        width: 412,
+        align: 'left',
+        columns: 1,
+        height: 500,
+        ellipsis: true
+      });
+
+    doc
+      .fillColor('black')
+      .font('Times-Roman', 12)
+      .moveDown()
+      .text("Education", {
+        width: 412,
+        align: 'left',
+        indent: 0,
+        columns: 1,
+        height: 500,
+        ellipsis: true
+      })
+
+    let education = [
+      'A.A., Liberal Arts: Science & Math, Coastline Community College',
+      'B.A., Bachelor of Arts: Education Sciences, University of California, Irvine',
+      'B.S., Bachelor of Science: Computer Science, Western Governors University'
+    ];
+
+    doc
+      .list(education, {
+        width: 412,
+        align: 'left',
+        indent: 15,
+        columns: 1,
+        height: 500,
+        ellipsis: true
+      })
+
+    doc
+      .moveUp(3)
+      .text("2018-2017", { align: 'right' })
+    doc.text("2020-2019", { align: 'right' })
+    doc.text("2021-2022", { align: 'right' })
 
 
     doc.end();
+
+    res.redirect('/user/profile')
+
 
   }
 }
