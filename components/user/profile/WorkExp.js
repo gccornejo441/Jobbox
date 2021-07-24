f2143064558ea3fd48f8813cdbc8d9c734d28d76
gccornejo441 <gcornejo441@gmail.com>
2021-07-23 22:35:50 -0700

    test commit

--- components/user/profile/WorkExp.js
+++ components/user/profile/WorkExp.js
@@ -125,7 +125,7 @@ const WorkExp = (props) => {
                                                     {duty}
                                                 </span>
                                             </div>
-                                        )
+                                        );
                                     })}
                                 </li>
                             </ul>
