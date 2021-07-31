20aea48f8c71a0d6b1ffa2cb4410fc66d4e515be
gccornejo441 <gcornejo441@gmail.com>
2021-07-31 13:43:30 -0700

    client side error due to mis handling of data in the case user submits a single skill into the db which converts it into a string

--- components/user/profile/Skills.js
+++ components/user/profile/Skills.js
@@ -3,12 +3,13 @@ const Skills = (props) => {
         <>
             {props.resume.map((data, index) => {
                 if (data.username === props.user.nickname) {
+                    console.log(typeof data.skills)
                     return (
                         <div key={index}>
                             <div className="border-b-4 mb-4 border-regal-blue">
                                 <h3 className="text-regal-blue text-3xl font-bold">Skills</h3>
                             </div>
-                            {data.skills !== null ? (data.skills.map((skill, index) => {
+                            {typeof data.skills != 'string' && data.skills !== null ? (data.skills.map((skill, index) => {
                                 return (
                                     <ul key={index} >
                                         <li className="bg-regal-blue text-white rounded-lg p-2 my-4 w-max tracking-widest">
@@ -17,7 +18,13 @@ const Skills = (props) => {
                                     </ul>
                                 );
                             })
-                            ) : ( "" )
+                            ) : (
+                                <ul>
+                                    <li className="bg-regal-blue text-white rounded-lg p-2 my-4 w-max tracking-widest">
+                                        {data.skills}
+                                    </li>
+                                </ul>
+                            )
                             }
                         </div>
                     );
